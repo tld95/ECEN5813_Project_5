de7408a5b8ee235aca6a8c172bed3a8713ada9c6
trist <trist@DESKTOP-IT1E9AD.home>
2019-09-30 23:42:35 -0700

    debug folder added with default including fb_run.axf.

---
+++ debug/main.d
@@ -0,0 +1,50 @@
+debug/main.o debug/main.d: source/main.c source/LED_control.h \
+ board/board.h board/clock_config.h drivers/fsl_common.h \
+ CMSIS/fsl_device_registers.h CMSIS/MKL25Z4.h CMSIS/core_cm0plus.h \
+ CMSIS/core_cmInstr.h CMSIS/cmsis_gcc.h CMSIS/core_cmFunc.h \
+ CMSIS/system_MKL25Z4.h CMSIS/MKL25Z4_features.h drivers/fsl_clock.h \
+ drivers/fsl_common.h drivers/fsl_gpio.h utilities/fsl_debug_console.h \
+ source/timing_control.h board/clock_config.h board/peripherals.h \
+ board/pin_mux.h CMSIS/MKL25Z4.h
+
+source/LED_control.h:
+
+board/board.h:
+
+board/clock_config.h:
+
+drivers/fsl_common.h:
+
+CMSIS/fsl_device_registers.h:
+
+CMSIS/MKL25Z4.h:
+
+CMSIS/core_cm0plus.h:
+
+CMSIS/core_cmInstr.h:
+
+CMSIS/cmsis_gcc.h:
+
+CMSIS/core_cmFunc.h:
+
+CMSIS/system_MKL25Z4.h:
+
+CMSIS/MKL25Z4_features.h:
+
+drivers/fsl_clock.h:
+
+drivers/fsl_common.h:
+
+drivers/fsl_gpio.h:
+
+utilities/fsl_debug_console.h:
+
+source/timing_control.h:
+
+board/clock_config.h:
+
+board/peripherals.h:
+
+board/pin_mux.h:
+
+CMSIS/MKL25Z4.h:
---
+++ debug/semihost_hardfault.d
@@ -0,0 +1,2 @@
+debug/semihost_hardfault.o debug/semihost_hardfault.d: \
+ source/semihost_hardfault.c
